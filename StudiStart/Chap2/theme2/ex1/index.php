f1d93c306b1752dca9168307b55f924cfed1b588
DroxKiwi <corentinfredj.pro@gmail.com>
2023-02-16 17:40:17 +0100

    adding new names and finishing php cours

---
+++ StudiStart/Chap2/theme2/ex1/index.php
@@ -0,0 +1,9 @@
+
+<?php
+$login = 'John';
+$messageCount = 0;
+$unreadMessagesCount = 28;
+$readMessageCount = 12;
+$messageCount = $readMessageCount + $unreadMessagesCount;
+echo 'Bonjour '.$login." ! Il y a ".$messageCount.' messages dans votre boîte de réception !';
+?>
